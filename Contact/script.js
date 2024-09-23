b582ae3181750b8dcdfe8ae4493db6b7776222e9
Yosia Sipahutar <yosia.535230117@.stu.untar.ac.id>
2024-09-23 19:35:35 +0700

    membuat tampilan kontak responsif dan menambahkan hamburger, masih jelek

---
+++ Contact/script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const burgerMenu = document.getElementById('burger-menu');
+    const navbarMenu = document.querySelector('.navbar-menu');
+    const closeButton = document.getElementById('close-button');
+
+    burgerMenu.addEventListener('click', () => {
+        navbarMenu.classList.toggle('active');
+        burgerMenu.classList.toggle('active');
+    });
+
+    closeButton.addEventListener('click', () => {
+        navbarMenu.classList.remove('active');
+        burgerMenu.classList.remove('active');
+    });
+});
