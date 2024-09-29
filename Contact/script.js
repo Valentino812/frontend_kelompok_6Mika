e521ce28bc461efaa89fb3599695d0e599c9cd4f
Valentino812 <untarvalen123@gmail.com>
2024-09-29 19:32:52 +0700

    sidebar for account and cart button in navbar (not done)

--- Contact/script.js
+++ Contact/script.js
@@ -2,6 +2,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const burgerMenu = document.getElementById('burger-menu');
     const navbarMenu = document.querySelector('.navbar-menu');
     const closeButton = document.getElementById('close-button');
+    const cartButton = document.getElementById('cart-button');
+    const accountButton = document.getElementById('account-button');
+    const sidebarCart = document.querySelector('.sidebar-cart');
+    const sidebarAccount = document.querySelector('.sidebar-account');
 
     burgerMenu.addEventListener('click', () => {
         navbarMenu.classList.toggle('active');
@@ -11,5 +15,17 @@ document.addEventListener('DOMContentLoaded', () => {
     closeButton.addEventListener('click', () => {
         navbarMenu.classList.remove('active');
         burgerMenu.classList.remove('active');
+        sidebarCart.classList.remove('active');
+        sidebarAccount.classList.remove('active');
     });
+
+    // 2.Account and cart sidebar:
+    cartButton.addEventListener('click', () => {
+        sidebarCart.classList.toggle('active');
+    });
+
+    accountButton.addEventListener('click', () => {
+        sidebarAccount.classList.toggle('active');
+    });
+
 });
