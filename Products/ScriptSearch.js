8b7aa8f5bddf7427b9a3441afd5455c2e2c6651c
andrewjohnsonwijaya <andrewjohnsonwijaya>
2024-09-30 18:49:29 +0700

    edit search product ui

--- Products/ScriptSearch.js
+++ Products/ScriptSearch.js
@@ -1,57 +1,45 @@
 const product = [
     {
         id: 0,
-        image: '../assets/s1.webp',
-        title: 'Cordmaster Domestic Black Tea Core',
-        price: 100000,
-     },
+        image: '../assets/shoes1.jpg',
+        title: 'Trailmaster Low "Tough Trails Tough Boots" - Cobalt Blue',
+        price: 3850000,
+    },
     {
         id: 1,
-        image: '../assets/s2.webp',
-        title: 'Tough Trailmaster - WaxedThe Boondockers - Waxed Flesh',
-        price: 200000,
+        image: '../assets/shoes2.jpg',
+        title: 'Legatant Old - Cloe Horserump Overdyed Dark Brown',
+        price: 5600000,
     },
-    {
-        id: 2,
-        image: '../assets/s3.webp',
-        title: 'The Boondockers Cloe Nuda - Naturale',
-        price: 30000,
-    },
-    {
-        id: 3,
-        image: '../assets/s4.webp',
-        title: 'The Shoe',
-        price: 40000,
-    }
 ];
 
-    const categories = [...new Set(product.map((item) => { return item }))]
+const categories = [...new Set(product.map((item) => { return item }))]
 
-    document.getElementById('searchBar').addEventListener('keyup', (e) => {
-        const searchData = e.target.value.toLowerCase();
-        const filteredData = categories.filter((item) => {
-            return (
-                item.title.toLowerCase().includes(searchData)
-            )
-        })
-        displayItem(filteredData)
-    });
+document.getElementById('searchBar').addEventListener('keyup', (e) => {
+    const searchData = e.target.value.toLowerCase();
+    const filteredData = categories.filter((item) => {
+        return (
+            item.title.toLowerCase().includes(searchData)
+        )
+    })
+    displayItem(filteredData)
+});
 
-    const displayItem = (items) => {
-        document.getElementById('root').innerHTML = items.map((item) => {
-            var { image, title, price } = item;
-            return (
-                `<div class='box'>
-                    <div class='img-box'>
-                        <img class='images' src=${image}></img>
-                    </div> 
-                    <div class='bottom'>
-                        <p>${title}</p>
-                        <h2>$ ${price}</h2>
-                        <button>Add to cart</button>
-                    </div>
-                </div>`
-            )
-        }).join('')
-    };
-    displayItem(categories);
+const displayItem = (items) => {
+    document.getElementById('root').innerHTML = items.map((item) => {
+        var { image, title, price } = item;
+        return (
+            `<div class='box'>
+                <div class='img-box'>
+                    <img class='images' src=${image}></img>
+                </div> 
+                <div class='bottom'>
+                    <p>${title}</p>
+                    <h2>Rp${price}</h2>
+                <button>Add to cart</button>
+                </div>
+            </div>`
+        )
+    }).join('')
+};
+displayItem(categories);
